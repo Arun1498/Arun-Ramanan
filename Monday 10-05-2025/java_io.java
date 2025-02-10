2afcb04021c7d35733eaf606bd0685c183cf8a5a
Arun1498 <142156827+Arun1498@users.noreply.github.com>
2025-02-10 14:13:13 +0530

    Add files via upload

---
+++ Monday 10-05-2025/java_io.java
@@ -0,0 +1,16 @@
+package mon;
+
+public class java_io {
+    public static void main(String[] args) {
+        System.out.print("print statement 1! ");
+        System.out.print("print statement 2! ");
+
+
+        System.out.println("println statement 1!");
+        System.out.println("println statement 2!");
+        System.out.println("println statement 3!");
+
+        System.err.print(("print statement error!!"));
+        System.err.println("println statement error!!");
+    }
+}
