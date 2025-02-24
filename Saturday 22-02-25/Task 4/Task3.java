07d6e0b11d60de60fd8b74a4a3f409e70ecab771
Arun1498 <142156827+Arun1498@users.noreply.github.com>
2025-02-24 17:38:40 +0530

    Update Task3.java

--- Saturday 22-02-25/Task 4/Task3.java
+++ Saturday 22-02-25/Task 4/Task3.java
@@ -1,12 +1,10 @@
-// 3. Write a Java program to create a method that reads a file and throws an exception if the file is not found.
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
-        String path = "C:\\Users\\gowth\\OneDrive\\Desktop\\TechM-Training\\11__21-02-2025 Task\\Task3\\SampleText.txt";
+        String path = "C:\Users\user\Desktop\TM Training\Saturday 22-02-25\\Task3\\Text.txt";
         try {
             FileRead(path);
         } catch (FileNotFoundException e) {
