249e1ebc25db67e37d28d20b58c9944b73112977
Arun1498 <142156827+Arun1498@users.noreply.github.com>
2025-02-24 17:54:44 +0530

    Update Task1.java

--- Saturday 22-02-25/Task 3/Task1.java
+++ Saturday 22-02-25/Task 3/Task1.java
@@ -1,19 +1,20 @@
 import java.io.File;
+
 public class Task1 {
     public static void main(String[] args) {
-        String Path = "C:\Users\user\Desktop\TM Training\Saturday 22-02-25";
+        String Path = "C:/Users/user/Desktop/TM Training/Saturday 22-02-25";
         File directory = new File(Path);
-        if(directory.isDirectory()){
+        if (directory.isDirectory()) {
             String[] files = directory.list();
-            if(files != null && files.length > 0){
-                System.out.println("The Files and Directory present in this path \""+Path+"\" are : ");
-                for(String file : files){
+            if (files != null && files.length > 0) {
+                System.out.println("The Files and Directory present in this path \"" + Path + "\" are : ");
+                for (String file : files) {
                     System.out.println(file);
                 }
-            }else{
+            } else {
                 System.out.println("The Directory is Empty !!!");
             }
-        }else{
+        } else {
             System.out.println("The given path is not a directory.");
         }
     }
