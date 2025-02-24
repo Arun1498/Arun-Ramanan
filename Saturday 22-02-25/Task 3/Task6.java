468d294772bbf13bf182ef1178dc2e107e5429f0
Arun1498 <142156827+Arun1498@users.noreply.github.com>
2025-02-24 17:58:17 +0530

    Update Task6.java

--- Saturday 22-02-25/Task 3/Task6.java
+++ Saturday 22-02-25/Task 3/Task6.java
@@ -1,9 +1,10 @@
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
 public class Task6 {
     public static void main(String[] args) {
-        String path = "C:\Users\user\Desktop\TM Training\Saturday 22-02-25\\Task3\\Task1.java";
+        String path = "C:/Users/user/Desktop/TM Training/Saturday 22-02-25/Task3/Task1.java";
         File file = new File(path);
         if (file.exists()) {
             long lastModified = file.lastModified();
