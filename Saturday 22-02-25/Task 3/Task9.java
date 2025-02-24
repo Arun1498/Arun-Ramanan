81f653757637680eec010f99998299b2ac32c086
Arun1498 <142156827+Arun1498@users.noreply.github.com>
2025-02-24 18:00:05 +0530

    Update Task9.java

--- Saturday 22-02-25/Task 3/Task9.java
+++ Saturday 22-02-25/Task 3/Task9.java
@@ -1,13 +1,14 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+
 public class Task9 {
     public static void main(String[] args) {
-        String path = "C:\Users\user\Desktop\TM Training\Saturday 22-02-25\\Task3\\Text.txt";
+        String path = "C:/Users/user/Desktop/TM Training/Saturday 22-02-25/Task3/Text.txt";
         File file = new File(path);
         if (file.exists() && file.isFile()) {
             try (FileInputStream fis = new FileInputStream(file)) {
-                byte[] byteArray = new byte[(int) file.length()]; 
+                byte[] byteArray = new byte[(int) file.length()];
                 int bytesRead = fis.read(byteArray);
                 System.out.println("File read successfully. Bytes read: " + bytesRead);
                 System.out.println("Byte Array Content:");
