46e6925dac4d89e8a948752805b5abedcb8a25e3
Arun1498 <142156827+Arun1498@users.noreply.github.com>
2025-02-24 17:53:37 +0530

    Update Task10.java

--- Saturday 22-02-25/Task 3/Task10.java
+++ Saturday 22-02-25/Task 3/Task10.java
@@ -1,16 +1,17 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+
 public class Task10 {
     public static void main(String[] args) {
-        String path = "C:\Users\user\Desktop\TM Training\Saturday 22-02-25\\Task3\\Text.txt";
-        try(BufferedReader br = new BufferedReader(new FileReader(path))){
-            String line ;
+        String path = "C:/Users/user/Desktop/TM Training/Saturday 22-02-25/Task3/Text.txt";
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
             System.out.println("File Content Read by Line by Line : ");
-            while((line = br.readLine()) != null){
+            while ((line = br.readLine()) != null) {
                 System.out.println(line);
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
