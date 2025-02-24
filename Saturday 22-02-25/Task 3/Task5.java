891b17c14ed65015eadadeac187585fc9477f0c8
Arun1498 <142156827+Arun1498@users.noreply.github.com>
2025-02-24 17:57:41 +0530

    Update Task5.java

--- Saturday 22-02-25/Task 3/Task5.java
+++ Saturday 22-02-25/Task 3/Task5.java
@@ -1,18 +1,18 @@
 import java.io.File;
+
 public class Task5 {
     public static void main(String[] args) {
-        String path1 = "C:\Users\user\Desktop\TM Training\Saturday 22-02-25";
-        String path = "C:\Users\user\Desktop\TM Training\Saturday 22-02-25\\Task3\\Task1.java";
+        String path1 = "C:/Users/user/Desktop/TM Training/Saturday 22-02-25";
+        String path = "C:/Users/user/Desktop/TM Training/Saturday 22-02-25/Task3/Task1.java";
         File fileOrDirectory = new File(path);
-        if(fileOrDirectory.exists()){
-            if(fileOrDirectory.isDirectory()){
+        if (fileOrDirectory.exists()) {
+            if (fileOrDirectory.isDirectory()) {
                 System.out.println("The File Path mentioned Above is Directory");
             }
-            if(fileOrDirectory.isFile()){
+            if (fileOrDirectory.isFile()) {
                 System.out.println("The File Path mentioned Above is A File!");
             }
-        }
-        else{
+        } else {
             System.out.println("No Directory Exists");
         }
     }
