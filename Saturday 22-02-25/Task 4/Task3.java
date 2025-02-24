cc0c7338f69631b5913caaffe2d641a1f21483f1
Arun1498 <142156827+Arun1498@users.noreply.github.com>
2025-02-24 17:49:04 +0530

    Update Task3.java

--- Saturday 22-02-25/Task 4/Task3.java
+++ Saturday 22-02-25/Task 4/Task3.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
-        String path = "C:\Users\user\Desktop\TM Training\Saturday 22-02-25\\Task3\\Text.txt";
+        String path = "C:\\Users\\user\\Desktop\\TM Training\\Saturday 22-02-25\\Task3\\Text.txt";
         try {
             FileRead(path);
         } catch (FileNotFoundException e) {
