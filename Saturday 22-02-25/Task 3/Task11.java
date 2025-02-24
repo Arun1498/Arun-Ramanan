d2006c0259d2f78d42f5eb87a9ddd067f8ca583a
Arun1498 <142156827+Arun1498@users.noreply.github.com>
2025-02-24 17:52:37 +0530

    Update Task11.java

--- Saturday 22-02-25/Task 3/Task11.java
+++ Saturday 22-02-25/Task 3/Task11.java
@@ -3,10 +3,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+
 public class Task11 {
     public static void main(String[] args) {
-        String path = "C:\Users\user\Desktop\TM Training\Saturday 22-02-25\\Task3\\Text.txt";
-        try{
+        String path = "C:\\Users\\user\\Desktop\\TM Training\\Saturday 22-02-25\\Task3\\Text.txt";
+        try {
             List<String> lineList = Files.readAllLines(Paths.get(path));
             System.out.println("File content:");
             for (String line : lineList) {
