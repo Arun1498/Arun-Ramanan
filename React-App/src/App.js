a79ce64247f386c2dc1d4fbff28b1d5dbfd5db68
Arun1498 <142156827+Arun1498@users.noreply.github.com>
2025-03-11 06:58:38 +0530

    Add files via upload

---
+++ React-App/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MovieList from "./Movielist"; 
+import "./App.css";
+
+
+function App() {
+  return (
+    <div className="App">
+      <MovieList />
+    </div>
+  );
+}
+
+export default App;
